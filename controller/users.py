618f2834afb84b4c1f18332146b7b50c00b91afa
coastby <whdpwl2@gmail.com>
2025-03-01 14:54:26 +0900

    add: controller 확장하기

---
+++ controller/users.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+  prefix="/users",
+  tags=["users"],
+  responses={404: {"description": "Not found"}},
+)
+
+@router.get("/{user_id}")
+def read_item(user_id: int):
+    return {"user_id": user_id}
